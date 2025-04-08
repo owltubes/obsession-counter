fbdd4021dcf669543fce0e9e60f463f7b2b380f6
owltubes <138794792+owltubes@users.noreply.github.com>
2025-04-08 10:14:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let countEl = document.getElementById("count-el")
 let saveEl = document.getElementById("save-el")
-count = 0
+let count = 0
 function increment() {
     count += 1
     countEl.textContent = count
